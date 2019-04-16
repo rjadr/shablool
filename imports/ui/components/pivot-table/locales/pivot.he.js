0e87f4899604a65b41f12a3f3c5546e9ce8f7a55
Joseph <maldjian11@gmail.com>
2019-04-15 22:32:15 -0500

    Change messages

--- imports/ui/components/pivot-table/locales/pivot.he.js
+++ imports/ui/components/pivot-table/locales/pivot.he.js
@@ -37,71 +37,71 @@
     });
     $.pivotUtilities.locales.he = {
       localeStrings: {
-        renderError: "אירעה שגיאה במהלך עיבוד ה- PivotTable.",
-        computeError: "ארעה שגיאה במהלך החישוב של PivotTable.",
-        uiRenderError: "ארעה שגיאה בזמן עיבוד של PivotTable.",
-        selectAll: "בחר את הכל",
-        selectNone: "אל תבחר כלום",
-        tooMany: "(ערכים רבים מידי)",
-        filterResults: "תוצאות סינון",
-        totals: "הכל",
-        vs: "של",
-        by: "לפי"
+        renderError: "An error occured while processing the- PivotTable.",
+        computeError: "There was an error during the calculation of the PivotTable.",
+        uiRenderError: "There was an error processing the PivotTable.",
+        selectAll: "Select all",
+        selectNone: "Select none",
+        tooMany: "(Too many entries)",
+        filterResults: "Filter results",
+        totals: "Everything",
+        vs: "of",
+        by: "as per"
       },
       aggregators: {
-        "כמות": tpl.count(frFmtInt),
-        "כמות ערכים ייחודיים": tpl.countUnique(frFmtInt),
-        "רשימת ערכים יחודיים": tpl.listUnique(", "),
-        "סכום": tpl.sum(frFmt),
-        "סכום שלם": tpl.sum(frFmtInt),
-        "ממוצע": tpl.average(frFmt),
-        "מינימום": tpl.min(frFmt),
-        "מקסימום": tpl.max(frFmt),
-        "סכום על סכום": tpl.sumOverSum(frFmt),
-        "סכום מעל 80%": tpl.sumOverSumBound80(true, frFmt),
-        "סכום מתחת 80%": tpl.sumOverSumBound80(false, frFmt),
-        "סכום כחלק מהכל": tpl.fractionOf(tpl.sum(), "total", frFmtPct),
-        "סכום כחלק מהשורות": tpl.fractionOf(tpl.sum(), "row", frFmtPct),
-        "סכום כחלק מהעמודות": tpl.fractionOf(tpl.sum(), "col", frFmtPct),
-        "כמות כוללת": tpl.fractionOf(tpl.count(), "total", frFmtPct),
-        "כמות שורות": tpl.fractionOf(tpl.count(), "row", frFmtPct),
-        "כמות עמודות": tpl.fractionOf(tpl.count(), "col", frFmtPct)
+        "Quantity": tpl.count(frFmtInt),
+        "Number of unique values": tpl.countUnique(frFmtInt),
+        "As list of unique values": tpl.listUnique(", "),
+        "Amount": tpl.sum(frFmt),
+        "Total amount": tpl.sum(frFmtInt),
+        "Average": tpl.average(frFmt),
+        "Minimum": tpl.min(frFmt),
+        "Maximum": tpl.max(frFmt),
+        "Amount on the sum": tpl.sumOverSum(frFmt),
+        "80% over": tpl.sumOverSumBound80(true, frFmt),
+        "80% under": tpl.sumOverSumBound80(false, frFmt),
+        "Set of different sizes": tpl.fractionOf(tpl.sum(), "total", frFmtPct),
+        "Sum as part of rows": tpl.fractionOf(tpl.sum(), "row", frFmtPct),
+        "Set of hard drives": tpl.fractionOf(tpl.sum(), "col", frFmtPct),
+        "Total quantity": tpl.fractionOf(tpl.count(), "total", frFmtPct),
+        "Number of rows": tpl.fractionOf(tpl.count(), "row", frFmtPct),
+        "Number of collumns": tpl.fractionOf(tpl.count(), "col", frFmtPct)
       },
       renderers: {
-        "טבלה": $.pivotUtilities.renderers["Table"],
-        "טבלת עמודות": $.pivotUtilities.renderers["Table Barchart"],
-        "מפת חום": $.pivotUtilities.renderers["Heatmap"],
-        "טבלת חום לפי שורות": $.pivotUtilities.renderers["Row Heatmap"],
-        "טבלת חום לפי עמודות": $.pivotUtilities.renderers["Col Heatmap"]
+        "Table": $.pivotUtilities.renderers["Table"],
+        "Collumn table": $.pivotUtilities.renderers["Table Barchart"],
+        "Heat map": $.pivotUtilities.renderers["Heatmap"],
+        "Heat table by line": $.pivotUtilities.renderers["Row Heatmap"],
+        "Heat table by collumn": $.pivotUtilities.renderers["Col Heatmap"]
       }
     };
     if (c3r) {
       $.pivotUtilities.c3_renderers = {
-        "טבלה": $.pivotUtilities.renderers["Table"],
-        "טבלת עמודות": $.pivotUtilities.renderers["Table Barchart"],
-        "מפת חום": $.pivotUtilities.renderers["Heatmap"],
-        "טבלת חום לפי שורות": $.pivotUtilities.renderers["Row Heatmap"],
-        "טבלת חום לפי עמודות": $.pivotUtilities.renderers["Col Heatmap"],
-        "גרף לינארי": c3r["Line Chart"],
-        "גרף עמודות": c3r["Bar Chart"],
-        "תרשים עמודות מוערמות": c3r["Stacked Bar Chart"],
-        "תרשים שטח": c3r["Area Chart"],
-        "תרשים פיזור": c3r["Scatter Chart"]
+        "Table": $.pivotUtilities.renderers["Table"],
+        "Collumn table": $.pivotUtilities.renderers["Table Barchart"],
+        "Heat map": $.pivotUtilities.renderers["Heatmap"],
+        "Heat map by row": $.pivotUtilities.renderers["Row Heatmap"],
+        "Heat map by collumn": $.pivotUtilities.renderers["Col Heatmap"],
+        "Linear graph": c3r["Line Chart"],
+        "Bar graph": c3r["Bar Chart"],
+        "Stacked bar chart": c3r["Stacked Bar Chart"],
+        "Area chart": c3r["Area Chart"],
+        "Scatter plot": c3r["Scatter Chart"]
       };
       $.pivotUtilities.locales.he.renderers = $.extend($.pivotUtilities.locales.he.renderers, $.pivotUtilities.c3_renderers);
     }
     if (gcr) {
       $.pivotUtilities.locales.he.gchart_renderers = {
-        "גרף לינארי": gcr["Line Chart"],
-        "גרף עמודות": gcr["Bar Chart"],
-        "תרשים עמודות מוערמות": gcr["Stacked Bar Chart"],
-        "תרשים שטח": gcr["Area Chart"]
+        "Linear graph": gcr["Line Chart"],
+        "Bar graph": gcr["Bar Chart"],
+        "Stacked bar graph": gcr["Stacked Bar Chart"],
+        "Area graph": gcr["Area Chart"]
       };
       $.pivotUtilities.locales.he.renderers = $.extend($.pivotUtilities.locales.he.renderers, $.pivotUtilities.locales.he.gchart_renderers);
     }
     if (d3r) {
       $.pivotUtilities.locales.he.d3_renderers = {
-        "תרשים פיזור": d3r["Treemap"]
+        "Scatter plot": d3r["Treemap"]
       };
       $.pivotUtilities.locales.he.renderers = $.extend($.pivotUtilities.locales.he.renderers, $.pivotUtilities.locales.he.d3_renderers);
     }
