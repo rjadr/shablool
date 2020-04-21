bf32f17cbeac8b1983df1da842afde6918be65f5
RJ Adriaansen <adriaansen@eshcc.eur.nl>
2020-04-22 00:53:45 +0200

    Update instructions.js

--- imports/ui/pages/game/player/instructions.js
+++ imports/ui/pages/game/player/instructions.js
@@ -8,8 +8,8 @@ const Instructions = () => (
     <div className="main-content">
       <div className="row">
         <div className="screen-main-instructions">
-          <h1 className="instructions-h"> אתה במשחק! </h1>
-          <h2 className="instructions-subheader instructions-h"> רואה את שמך על המסך? </h2>
+          <h1 className="instructions-h"> You have entered the game! </h1>
+          <h2 className="instructions-subheader instructions-h"> Do your see your name on the screen? </h2>
         </div>
       </div>
     </div>
